8b33263233a86d953ad4bb3f204e964f306d77d6
sfskysf <67379914+sfskysf@users.noreply.github.com>
2021-08-27 04:09:21 +0200

    day 9

---
+++ js/animations/contentAnimations.js
@@ -0,0 +1,179 @@
+
+function setUpanimations() {
+
+    animateNavbar();
+    navbarActiveTracker();
+    animateLanding();
+    animateMyTitles();
+    animateWhatIdo();
+    animateMyWorks();
+    animateTechStack();
+
+}
+
+function animateNavbar() {
+    gsap.to('.navbar', {
+        scrollTrigger: {
+            trigger: '.landing-text',
+            start: 'top +=100',
+            id: 'nav',
+            toggleActions: "play none none reverse",
+            // markers: true
+        },
+
+        background: '#242424',
+        duration: 0.4
+    });
+
+}
+
+async function navbarActiveTracker() {
+    gsap.utils.toArray('.sec').forEach((item, i) => {
+        let navItem = '#' + item.id + 'Nav';
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: item,
+                start: 'top 60%',
+                end: 'bottom 30%',
+                toggleActions: "play reverse play reverse"
+            }
+        }).to(navItem, {
+
+            color: "#FF0000",
+            duration: 0.5,
+            ease: 'power3'
+        }).to(navItem, {
+            fontWeight: 600,
+            duration: 0.2,
+            ease: 'power4'
+        }).to(navItem, {
+            borderBottom: '2px solid red',
+            duration: 0.2
+        }, '-=0.2');
+
+    });
+}
+
+function animateLanding() {
+    gsap.timeline().from('.landing-text h3', { delay: 1, duration: 0.5, scaleX: 0, ease: "back" })
+        .to('.landing-text .animgrp', { duration: 1.5, clipPath: "polygon(0 0, 100% 0%, 100% 100%, 0 100%)", y: '30' })
+        .to('.landing-text .animgrp .bold-red', { duration: 1.5, opacity: 1 })
+        .add(async function () { particlesJS.load('landing', '/new/js/practicles/particles.json'); })
+        .from('.scroll-icon', { duration: 1, opacity: 0, ease: "back", x: -50 }, '-=1.5')
+        .from('.landing-container .social-icons', { duration: 1, opacity: 0, ease: "back", x: 50 }, '-=1.5');
+
+
+}
+
+function animateMyTitles() {
+    gsap.utils.toArray('#myTitles').forEach((item, i) => {
+        gsap.timeline({ scrollTrigger: { trigger: item, start: 'top 90%' } })
+            .from(item, { x: (i % 2 === 0) ? 300 : -300, opacity: 0, duration: 2, ease: Power4.in })
+            .from(item.querySelector('.myTitlesIcon'), { x: (i % 2 === 0) ? 50 : -50, rotation: (i % 2 === 0) ? 360 : -360, duration: 1 }, '-=1');
+
+    });
+
+}
+
+function animateWhatIdo() {
+    gsap.from('.whatdo .whatdo-text p', {
+        scrollTrigger: {
+            trigger: '.whatdo .whatdo-text p',
+            start: 'top bottom',
+            toggleActions: "play none none reverse",
+        }, opacity: 0, x: 100, y: 80, duration: 1.5
+    });
+
+
+    gsap.from('.whatDoCard', {
+        scrollTrigger: {
+            trigger: '.whatDoCards',
+            start: 'top 60%',
+            end: 'bottom 85%',
+
+            scrub: 2,
+            id: 'whatdocards',
+            // markers: true
+        },
+        y: 400,
+        opacity: 0,
+        stagger: 1,
+        duration: 1
+    });
+
+}
+
+function animateMyWorks() {
+    gsap.utils.toArray('.project-item').forEach((item, i) => {
+        // i = 2 is hardcoded as the last element for now.
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: item,
+                start: 'bottom bottom',
+                end: (i == 2) ? 'top bottom' : 'bottom top',
+                // toggleActions: "play none none none",
+                pin: true,
+                pinSpacing: false,
+                scrub: 0.5,
+                // markers: true,
+                id: 'projects'
+            }
+        });
+    });
+
+
+}
+
+function animateTechStack() {
+    gsap.from('.techSkills', {
+        scrollTrigger: {
+            trigger: '.techSkills',
+            start: 'top center',
+            end: 'top 40%',
+            scrub: 2,
+
+        },
+        scale: 0.7,
+        borderRadius: '2%',
+        duration: 0.5,
+    });
+
+}
+
+function animateAboutMe() {
+
+
+}
+
+function animateContact() {
+
+
+}
+
+
+function pageTransition(namespace) {
+    let tl = gsap.timeline();
+    tl.to(".loading-screen", {
+        duration: 1.2,
+        width: "100%",
+        left: "0%",
+        ease: "Expo.easeInOut",
+    });
+
+    tl.to(".loading-screen", {
+        duration: 0.8,
+        width: "100%",
+        left: "100%",
+        ease: "Expo.easeInOut",
+        delay: 0.3,
+    });
+    tl.set(".loading-screen", { left: "-100%" });
+}
+
+
+const cleanGSAP = () => {
+    ScrollTrigger.getAll().forEach(t => t.kill(false));
+    ScrollTrigger.refresh();
+    window.dispatchEvent(new Event("resize"));
+};
--- js/main.js
+++ js/main.js
@@ -28,7 +28,6 @@ let scrollY = 0;
 
 
 window.onload = async function () {
-  gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
   barba.init({
     sync: true,
@@ -60,9 +59,10 @@ window.onload = async function () {
       },
 
       async enter(data) {
-       data.next.namespace === "home" ? cleanGSAP() : window.scrollTo(0, 0); 
+        data.next.namespace === "home" ? cleanGSAP() : window.scrollTo(0, 0);
 
         // contentAnimation();
+
       },
 
       async once(data) {
@@ -84,8 +84,8 @@ window.onload = async function () {
 
       },
 
-      afterEnter: function (data) {
-        animate();
+      afterEnter: async function (data) {
+        setUpanimations();
       }
     },
     {
@@ -97,22 +97,6 @@ window.onload = async function () {
   });
 
 
-
-  width = window.innerWidth;
-
-  if (width < 450) {
-    pin = '1px';
-
-    const techTags = document.querySelectorAll(".tech-tag");
-    const contactTitle = document.querySelector(".contact-container");
-    let i = 0
-    document.querySelectorAll('.project-image').forEach(elem => {
-      techTags[i].after(elem);
-      i++;
-    });
-
-    contactTitle.before(document.querySelector('.reviews-container'));
-  }
 };
 
 
@@ -152,113 +136,6 @@ async function copyEmail() {
 
 }
 
-function animate() {
-
-  const landingH1 = document.querySelector('.landing-text h1');
-  const landingH3 = document.querySelector('.landing-text .animgrp');
-  const fs = document.querySelector('.landing-text .animgrp .bold-red');
-
-  gsap.to('.navbar', {
-    scrollTrigger: {
-      trigger: '.landing-text',
-      start: 'top +=100',
-      id: 'nav',
-      toggleActions: "play none none reverse",
-      // markers: true
-    },
-
-    background: '#242424',
-    duration: 0.4
-  });
-
-  const tl = gsap.timeline();
-
-
-  tl.from(landingH1, { delay: 1, duration: 0.5, scaleX: 0, ease: "back" })
-    .to(landingH3, { duration: 1.5, clipPath: "polygon(0 0, 100% 0%, 100% 100%, 0 100%)", y: '30' })
-    .to(fs, { duration: 1.5, opacity: 1 })
-    .add(async function () { particlesJS.load('landing', '/new/js/practicles/particles.json'); })
-    .from('.landing-btn', { duration: 1, opacity: 0, stagger: -0.5, ease: "back", x: -500 }, '-=1')
-    .from('.landing-container .social-icons', { duration: 1, opacity: 0, ease: "back", x: 500 }, '-=1.5');
-
-  const tl2 = gsap.timeline({
-    scrollTrigger: {
-      trigger: '.whatdo-text',
-      start: 'top bottom',
-      end: 'top 200px',
-
-      toggleActions: "play none none reverse",
-      id: 'whatdo',
-      // markers: true
-    }
-  });
-
-  tl2.from('.whatdo h4', { x: -300, opacity: 0, duration: 3, ease: Power1.inOut })
-    .from('.whatdo .whatdo-text, .whatdo .brackets', { y: 300, opacity: 0, duration: 0.7, ease: 'power1' }, '-=2.5');
-
-
-
-
-  gsap.from('.whatDoCard', {
-    scrollTrigger: {
-      trigger: '.whatDoCards',
-      start: 'top bottom',
-      end: 'top 70%',
-
-      scrub: 2,
-      id: 'whatdocards',
-      // markers: true
-    },
-    y: 400,
-    opacity: 0,
-    stagger: 1,
-    duration: 1
-  });
-
-  gsap.from('.myworks', {
-    scrollTrigger: {
-      trigger: '.myworks',
-      start: 'top center'
-    },
-    opacity: 0,
-    duration: 1.5,
-    ease: Power4.inOut
-  });
-
-  gsap.utils.toArray('.project-item').forEach((item, i) => {
-    // i = 2 is hardcoded as the last element for now.
-    gsap.timeline({
-      scrollTrigger: {
-        trigger: item,
-        start: 'bottom bottom',
-        end: (i == 2) ? 'top bottom' : 'bottom top',
-        // toggleActions: "play none none none",
-        pin: true,
-        pinSpacing: false,
-        scrub: 0.5,
-        // markers: true,
-        id: 'projects'
-      }
-    });
-  });
-
-
-  gsap.from('.techSkills', {
-    scrollTrigger: {
-      trigger: '.techSkills',
-      start: 'top center',
-      end: 'top 40%',
-      scrub: 2,
-
-    },
-    scale: 0.7,
-    borderRadius: '2%',
-    duration: 0.5,
-  });
-
-
-
-}
 
 
 function delay(n) {
@@ -270,34 +147,51 @@ function delay(n) {
   });
 }
 
-function pageTransition(namespace) {
-  let tl = gsap.timeline();
-  tl.to(".loading-screen", {
-    duration: 0.8,
-    width: "100%",
-    left:  "0%",
-    ease: "Expo.easeInOut",
-  });
 
-  tl.to(".loading-screen", {
-    duration: 0.6,
-    width: "100%",
-    left:  "100%" ,
-    ease: "Expo.easeInOut",
-    delay: 0.3,
+
+
+
+let form = document.querySelector("#contactForm");
+form.addEventListener("submit", helloBrozzer);
+
+async function helloBrozzer(event) {
+
+  event.preventDefault();
+
+  let status = document.querySelector("#successmsg");
+
+  fetch("https://formspree.io/f/mknkpdej", {
+    method: "POST",
+    body: new FormData(event.target),
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(response => {
+    status.innerHTML = "Thanks, I'll get back to you soon!";
+    form.reset()
+  }).catch(error => {
+    status.innerHTML = "Error, something went wrong";
   });
-  tl.set(".loading-screen", { left: "-100%" });
 }
 
-// function contentAnimation() {
-//   var tl = gsap.timeline();
-//   tl.from(".animate-this", { duration: 1, y: 30, opacity: 0, stagger: 0.4, delay: 0.2 });
-// }
+
+let burgerBtn = document.querySelector(".navbar-toggler");
+
+burgerBtn.addEventListener("click", toggleMenu);
+
+let menuAnim = null;
+async function toggleMenu() {
+  smallNav = document.querySelector('.smallNav');
+  
+  if(menuAnim == null){
+  menuAnim = gsap.timeline().to(smallNav, { duration: 1, display: "flex", clipPath: "polygon(0 0, 100% 0, 100% 14%, 0 14%)" })
+    .from(smallNav, { duration: 1, right: '-100%' }, '-=1.1')
+    .to(smallNav, { duration: 0.6, clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)", ease: 'power3' });
+
+  }else{
+    menuAnim.play();
+  }
+  setTimeout(() => { menuAnim.reverse();}, 4000); //close simulation
 
 
-
-const cleanGSAP = () => {
-  ScrollTrigger.getAll().forEach(t => t.kill(false));
-  ScrollTrigger.refresh();
-  window.dispatchEvent(new Event("resize"));
-};
+}
