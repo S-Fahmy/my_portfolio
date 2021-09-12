8c0e2e1bdd7a399845a1f0db870a294b09b0bce6
Sf <saleh.fahmy2@gmail.com>
2021-09-13 00:29:04 +0200

    d

--- js/main.js
+++ js/main.js
@@ -1,97 +1,88 @@
-
 async function scrollToSection(sectionId) {
   gsap.to(window, { duration: 1, scrollTo: { y: sectionId, offsetY: 120 } });
 }
 
 let scrollY = 0;
 
-
-
 window.onload = async function () {
-
   barba.init({
     sync: true,
-    transitions: [{
-      name: 'home',
+    transitions: [
+      {
+        name: "home",
 
-      async leave(data) {
-        const done = this.async();
-        pageTransition(data.current.namespace);
-        await delay(800);        
-        done();
+        async leave(data) {
+          const done = this.async();
+          pageTransition(data.current.namespace);
+          await delay(800);
+          done();
+        },
+
+        async enter(data) {
+          data.next.namespace === "home"
+            ? cleanGSAP()
+            : gsap.to(window, { scrollTo: { y: 0 } });
+        },
       },
+    ],
+    views: [
+      {
+        namespace: "home",
+        beforeLeave: async function (data) {
+          scrollY = barba.history.current.scroll.y;
+        },
+        beforeEnter: async function () {
+          if (scrollY > 0) {
+            window.scrollTo(0, scrollY);
+          }
+        },
 
-      async enter(data) {
-        data.next.namespace === "home" ? cleanGSAP() : gsap.to(window, {scrollTo: {y: 0}});
-
+        afterEnter: function (data) {
+          setUpanimations();
+        },
       },
-
-
-    }],
-    views: [{
-      namespace: 'home',
-      beforeLeave: async function (data) {
-        scrollY = barba.history.current.scroll.y;
-
+      {
+        namespace: "projects",
       },
-      beforeEnter: async function () {
-        if (scrollY > 0) {
-          window.scrollTo(0, scrollY);
-        }
-
-      },
-
-      afterEnter: function (data) {
-        setUpanimations();
-      }
-    },
-    {
-      namespace: 'projects',
-    }]
+    ],
   });
 
-
   if (window.innerWidth < 1000) {
-    gsap.to('.navbar-brand', {
+    gsap.to(".navbar-brand", {
       scrollTrigger: {
-        trigger: '.landing-text',
-        start: 'top 5%',
-        scrub: 2
+        trigger: ".landing-text",
+        start: "top 5%",
+        scrub: 2,
       },
-      opacity: 0, x: -100, duration: 2
+      opacity: 0,
+      x: -100,
+      duration: 2,
     });
   }
-
 };
 
-
-let activeTabId = '#langs';
+let activeTabId = "#langs";
 
 async function displayTab(e, tabName) {
+  document.querySelector(".tech-tabs .active").classList.remove("active");
 
-  document.querySelector('.tech-tabs .active').classList.remove('active');
+  document.querySelector(activeTabId).classList.add("hidden");
 
-  document.querySelector(activeTabId).classList.add('hidden');
+  e.target.classList.add("active");
 
-  e.target.classList.add('active');
-
-  document.querySelector(tabName).classList.remove('hidden');
+  document.querySelector(tabName).classList.remove("hidden");
 
   activeTabId = tabName;
-
 }
 
 async function copyEmail() {
+  await navigator.clipboard.writeText("saleh.fahmy2@gmail.com");
 
-  await navigator.clipboard.writeText('saleh.fahmy2@gmail.com');
+  const copyBtn = document.querySelector(".copy-btn");
 
-  const copyBtn = document.querySelector('.copy-btn');
+  copyBtn.textContent = "Copied!";
 
-  copyBtn.textContent = 'Copied!'
-
-  setTimeout(() => copyBtn.textContent = 'Copy', 2000);
-
-
+  setTimeout(() => (copyBtn.textContent = "Copy"), 2000);
 }
 
 function delay(n) {
@@ -107,7 +98,6 @@ let form = document.querySelector("#contactForm");
 form.addEventListener("submit", mailSubmit);
 
 async function mailSubmit(event) {
-
   event.preventDefault();
 
   let status = document.querySelector("#successmsg");
@@ -116,12 +106,14 @@ async function mailSubmit(event) {
     method: "POST",
     body: new FormData(event.target),
     headers: {
-      'Accept': 'application/json'
-    }
-  }).then(response => {
-    status.innerHTML = "Thanks, I'll get back to you soon!";
-    form.reset()
-  }).catch(error => {
-    status.innerHTML = "Error, something went wrong";
-  });
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      status.innerHTML = "Thanks, I'll get back to you soon!";
+      form.reset();
+    })
+    .catch((error) => {
+      status.innerHTML = "Error, something went wrong";
+    });
 }
